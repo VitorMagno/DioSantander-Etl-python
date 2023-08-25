90c9e211441c05d0a252ce850d64c307b3d3e1b5
Vitor Magno <vmcg@ic.ufal.br>
2023-08-25 11:37:53 -0300

    cleaning and exporting

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # DioSantander-Etl-python
 - ETL simples de um csv de animes para o desafio do bootcamp de data science santander+dio
+- [link](https://www.kaggle.com/datasets/CooperUnion/anime-recommendations-database) para o dataset
--- etl-python.py
+++ etl-python.py
@@ -15,6 +15,7 @@ df_rating
 df_anime_filtrado.sort_values(by='members', ascending=False).head(10)
 # %%
 df_anime_filtrado.sort_values(by='rating', ascending=False).head(10)
+df_anime_filtrado = df_anime_filtrado.dropna(subset=['rating'])
 # %%
 df_user_rating_anime = pd.merge(left=df_rating, right=df_anime_filtrado, on='anime_id',how='left')
 columns=['user_id','anime_id','rating_x','name']
@@ -22,4 +23,11 @@ df_user_rating_anime = df_user_rating_anime[columns]
 #%%
 df_user_rating_anime.rename(columns={"rating_x": "user_rating"},inplace=True)
 # %%
-df_user_rating_anime.head(10)
+df_user_rating_anime = df_user_rating_anime.dropna(subset=['name'])
+#%%
+df_anime_filtrado.to_csv("./data/general_rating.csv")
+df_user_rating_anime.to_csv("./data/user_rating.csv")
+# %%
+df_anime_filtrado
+#%%
+df_user_rating_anime
